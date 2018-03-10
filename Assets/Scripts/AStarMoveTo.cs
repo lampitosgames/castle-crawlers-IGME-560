50301d5195d54a0e24fb7300cbcd59b96efec36c
Daniel Timko <dantimko1@gmail.com>
2018-03-09 20:32:50 -0500

    Added an A* character

---
+++ Assets/Scripts/AStarMoveTo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AStarMoveTo : MonoBehaviour
+{
+	public Transform goalPosition;
+	public Material lineMaterial;
+
+	NavMeshAgent agent = null;
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent> ();
+		agent.destination = goalPosition.position;
+	}
+
+	void Update ()
+	{
+		if (agent == null || agent.path == null) {
+			return;
+		}
+
+		LineRenderer line = this.GetComponent<LineRenderer> ();
+		if (line == null) {
+			line = this.gameObject.AddComponent<LineRenderer> ();
+			line.material = lineMaterial;
+			line.SetWidth (0.2f, 0.2f);
+			line.SetColors (Color.cyan, Color.cyan);
+		}
+
+		NavMeshPath path = agent.path;
+
+		line.positionCount = path.corners.Length;
+		for (int i = 0; i < path.corners.Length; i++) {
+			line.SetPosition (i, path.corners [i] + new Vector3 (0.0f, 1.0f, 0.0f));
+		}
+	}
+
+}
