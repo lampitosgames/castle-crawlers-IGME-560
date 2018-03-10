f89bd59d5b28fd3bd2cced907ec55436da05f738
Daniel Timko <dantimko1@gmail.com>
2018-03-10 16:38:30 -0500

    Made the target location for the AStarCharacter moveable with the first person controller

---
+++ Assets/Scripts/AStarTargetSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class AStarTargetSelect : MonoBehaviour {
+
+	public Transform aStarTargetGameObject;
+	public GameObject aStarCharacter;
+	
+	// Update is called once per frame
+	void Update() {
+		//Detect left click
+		if (Input.GetMouseButtonDown(0)) {
+			//Raycast from camera.  Collision sets the target of the A* algorithm
+			Vector3 forward = GetComponent<Camera>().transform.forward;
+
+			RaycastHit hit;
+			Physics.Raycast(transform.position, forward, out hit, 1000.0f);
+			if (hit.point != null) {
+				aStarTargetGameObject.position = hit.point;
+				aStarCharacter.GetComponent<NavMeshAgent>().destination = hit.point;
+			}
+		}
+	}
+}
